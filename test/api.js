66c5a55b7f5563f760db375ad1810acfb6fdeda3
Vincent Weevers <mail@vincentweevers.nl>
2019-03-24 12:18:37 +0100

    New naming scheme

--- test/api.js
+++ test/api.js
@@ -11,7 +11,32 @@ test('build with current node version', function (t) {
     t.ifError(err)
     t.doesNotThrow(function () {
       var folder = os.platform() + '-' + os.arch()
-      var name = 'node-' + process.versions.modules + '.node'
+      var name = 'node.abi' + process.versions.modules + '.node'
+      var addon = require(path.join(__dirname, 'package', 'prebuilds', folder, name))
+      t.equal(addon.check(), 'prebuildify')
+    })
+    t.end()
+  })
+})
+
+test('uv, armv and libc tags', function (t) {
+  prebuildify({
+    cwd: path.join(__dirname, 'package'),
+    targets: [{runtime: 'node', target: process.version}],
+    tagUv: 123,
+    tagArmv: true, // Should be excluded (unless you run these tests on ARM)
+    tagLibc: true // Should be glibc (unless you run these tests on Alpine)
+  }, function (err) {
+    t.ifError(err)
+    t.doesNotThrow(function () {
+      var folder = os.platform() + '-' + os.arch()
+      var name = [
+        'node',
+        'abi' + process.versions.modules,
+        'uv123',
+        'glibc',
+        'node'
+      ].join('.')
       var addon = require(path.join(__dirname, 'package', 'prebuilds', folder, name))
       t.equal(addon.check(), 'prebuildify')
     })
