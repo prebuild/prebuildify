20d030b85336e55d52e8b50fa6a9e56ca3feeaee
aminya <aminyahyaabadi74@gmail.com>
2020-10-20 03:42:53 -0500

    Revert "update mkdirp"
    This reverts commit a81f2ba67a61aec08312161fb5af287e5a1f60d7.

--- index.js
+++ index.js
@@ -63,14 +63,15 @@ function prebuildify (opts, cb) {
   // over the local .bin folder. Counter that by (again) adding .bin to PATH.
   opts.env = npmRunPath.env({ env: opts.env, cwd: opts.cwd })
 
-  mkdirp(opts.builds).then(function () {
+  mkdirp(opts.builds, function (err) {
+    if (err) return cb(err)
     loop(opts, function (err) {
       if (err) return cb(err)
 
       if (opts.artifacts) return copyRecursive(opts.artifacts, opts.builds, cb)
       return cb()
     })
-  }).catch(function (err) { return cb(err) })
+  })
 }
 
 function loop (opts, cb) {
@@ -196,7 +197,7 @@ function build (target, runtime, opts, cb) {
     args.push('--release')
   }
 
-  mkdirp(cache).then(function () {
+  mkdirp(cache, function () {
     var child = proc.spawn(opts.nodeGyp, args, {
       cwd: opts.cwd,
       env: opts.env,
@@ -215,7 +216,7 @@ function build (target, runtime, opts, cb) {
         })
       })
     })
-  }).catch(function (err) { return console.warn(err) })
+  })
 }
 
 function findBuild (dir, cb) {
