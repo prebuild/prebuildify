prebuild/prebuildify

commit 7b6dcbd0860a82db8804079ba55663affd9ae555

tag prebuilds with pkg name

index.js
@@ -67,13 +67,16 @@ function prebuildify (opts, cb) {
   // over the local .bin folder. Counter that by (again) adding .bin to PATH.
   opts.env = npmRunPath.env({ env: opts.env, cwd: opts.cwd })
 
-  mkdirp(opts.builds, function (err) {
+  addName(opts, function (err) {
     if (err) return cb(err)
-    loop(opts, function (err) {
+    mkdirp(opts.builds, function (err) {
       if (err) return cb(err)
+      loop(opts, function (err) {
+        if (err) return cb(err)
 
-      if (opts.artifacts) return copyRecursive(opts.artifacts, opts.builds, cb)
-      return cb()
+        if (opts.artifacts) return copyRecursive(opts.artifacts, opts.builds, cb)
+        return cb()
+      })
     })
   })
 }
@@ -108,12 +111,27 @@ function loop (opts, cb) {
   })
 }
 
-function prebuildName (target, opts) {
-  var tags = [target.runtime]
+function addName (opts, cb) {
+  if (opts.name) return cb()
+  fs.readFile(path.join(opts.cwd, 'package.json'), 'utf-8', function (err, pkg) {
+    if (err) return cb()
+    try {
+      opts.name = JSON.parse(pkg).name
+    } catch (err) {
+      // do nothing
+    }
+    cb()
+  })
+}
 
-  if (opts.napi) {
-    tags.push('napi')
-  } else {
+function encodeName (name) {
+  return name.replace(/\//g, '+')
+}
+
+function prebuildName (target, opts) {
+  var tags = [encodeName(opts.name || target.runtime)]
+
+  if (!opts.napi) {
     tags.push('abi' + abi.getAbi(target.target, target.runtime))
   }
 

test/api.js
@@ -12,7 +12,7 @@ test('build with current node version', function (t) {
     t.ifError(err)
     t.doesNotThrow(function () {
       var folder = os.platform() + '-' + os.arch()
-      var name = 'node.abi' + process.versions.modules + '.node'
+      var name = 'addon.abi' + process.versions.modules + '.node'
       var addon = require(path.join(__dirname, 'package', 'prebuilds', folder, name))
       t.equal(addon.check(), 'prebuildify')
     })
@@ -25,14 +25,14 @@ test('uv, armv and libc tags', function (t) {
     cwd: path.join(__dirname, 'package'),
     targets: [{ runtime: 'node', target: process.version }],
     tagUv: 123,
-    tagArmv: true, // Should be excluded (unless you run these tests on ARM)
+    tagArmv: false, // Should be excluded (unless you run these tests on ARM)
     tagLibc: true // Should be glibc (unless you run these tests on Alpine)
   }, function (err) {
     t.ifError(err)
     t.doesNotThrow(function () {
       var folder = os.platform() + '-' + os.arch()
       var name = [
-        'node',
+        'addon',
         'abi' + process.versions.modules,
         'uv123',
         'glibc',

bin.js
@@ -5,6 +5,7 @@ var prebuildify = require('./index')
 
 var argv = minimist(process.argv.slice(2), {
   alias: {
+    name: 'n',
     target: 't',
     version: 'v',
     all: 'a',

commit 2bc7411e4da9b7d6ae91ad35143f3bcc4a2cbe43

(fix) Ensure runtime and napi are emitted as needed.
Per issue #79 there seems to be a regression introduced
in 7b6dcbd which caused the `target.runtime` to no longer
be emitted when a name is used. A name is also _always_
used even if not passed, as it will grab a name from the
`package.json`. This commit also dropped including the
`napi` tag, which is still utilized downstream.

This change fixes the regression and follows this logic;

1. Emit `name` as a tag (this will always be true due to
`addName` function logic)
2. Emit `runtime` as a tag always
3. Emit `napi` as a tag if option enabled (likely always true,
as it if defaulted to true)

index.js
@@ -129,9 +129,17 @@ function encodeName (name) {
 }
 
 function prebuildName (target, opts) {
-  var tags = [encodeName(opts.name || target.runtime)]
+  var tags = []
 
-  if (!opts.napi) {
+  if (opts.name) {
+    tags.push(encodeName(opts.name))
+  }
+
+  tags.push(target.runtime)
+
+  if (opts.napi) {
+    tags.push('napi')
+  } else {
     tags.push('abi' + abi.getAbi(target.target, target.runtime))
   }
 

test/api.js
@@ -12,7 +12,31 @@ test('build with current node version', function (t) {
     t.ifError(err)
     t.doesNotThrow(function () {
       var folder = os.platform() + '-' + os.arch()
-      var name = 'addon.abi' + process.versions.modules + '.node'
+      var name = 'addon.node.abi' + process.versions.modules + '.node'
+      var addon = require(path.join(__dirname, 'package', 'prebuilds', folder, name))
+      t.equal(addon.check(), 'prebuildify')
+    })
+    t.end()
+  })
+})
+
+test('runtime napi flags', function (t) {
+  prebuildify({
+    cwd: path.join(__dirname, 'package'),
+    targets: [{ runtime: 'node', target: process.version }],
+    napi: true,
+    tagLibc: true // Should be glibc (unless you run these tests on Alpine)
+  }, function (err) {
+    t.ifError(err)
+    t.doesNotThrow(function () {
+      var folder = os.platform() + '-' + os.arch()
+      var name = [
+        'addon',
+        'node',
+        'napi',
+        'glibc',
+        'node'
+      ].join('.')
       var addon = require(path.join(__dirname, 'package', 'prebuilds', folder, name))
       t.equal(addon.check(), 'prebuildify')
     })
@@ -33,6 +57,7 @@ test('uv, armv and libc tags', function (t) {
       var folder = os.platform() + '-' + os.arch()
       var name = [
         'addon',
+        'node',
         'abi' + process.versions.modules,
         'uv123',
         'glibc',